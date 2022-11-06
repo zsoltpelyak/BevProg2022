75b5636db95586d5bdb97b6d63ceeae05fceee61
zsoltpelyak <pelyakzsolt03@gmail.com>
2022-11-06 15:32:16 +0100

    szorzotabla hf

---
+++ szorzotabla.py
@@ -0,0 +1,9 @@
+for i in range(1, 13):
+    print(i, end="\t")
+print()
+print("______________________________________________\n")
+
+for j in range(1, 13):
+    for k in range(1, 13):
+        print(j * k, end ="\t")
+    print("\n")
